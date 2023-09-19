6227a3b1d1c1632b865f87c1edbf02e94b9b5917
vkhrebtov <vkhrebtov@forexware.com>
2023-09-19 21:10:33 +0300

    refactor, add post test

--- __test__/e2e/videos.api.test.ts
+++ __test__/e2e/videos.api.test.ts
@@ -13,4 +13,39 @@ describe('/videos', () => {
   it('POST /videos should not create video with incorrect data', async () => {
     await request(app).post('/videos').send({title: 'test_title'}).expect(400)
   })
+
+  it('POST /videos should create new video, use GET /videos/:id to check created video', async () => {
+    const postResponse = await request(app)
+      .post('/videos')
+      .send({title: 'title', author: 'author', availableResolutions: ['P240']})
+      .expect(201)
+
+    const createdVideo = postResponse.body
+
+    expect(createdVideo).toEqual({
+      title: 'title',
+      author: 'author',
+      id: expect.any(Number),
+      availableResolutions: ['P240'],
+      createdAt: expect.any(String),
+      publicationDate: expect.any(String),
+      minAgeRestriction: null,
+      canBeDownloaded: false,
+    })
+
+    const getResponse = await request(app).get('/videos').expect(200)
+
+    expect(getResponse.body).toEqual([
+      {
+        title: 'title',
+        author: 'author',
+        id: expect.any(Number),
+        availableResolutions: ['P240'],
+        createdAt: expect.any(String),
+        publicationDate: expect.any(String),
+        minAgeRestriction: null,
+        canBeDownloaded: false,
+      },
+    ])
+  })
 })
