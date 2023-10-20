e28dc41ad76c6919c109a35a229789595085c703
vkhrebtov <vkhrebtov@forexware.com>
2023-10-20 16:41:51 +0300

    rm await for jwt.sign

---
+++ src/application/crypto-service.ts
@@ -0,0 +1,43 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import {SETTINGS} from '../shared/configs'
+import crypto from 'crypto'
+
+const TOKEN_EXPIRES_IN = '1h'
+
+export const cryptoService = {
+  getJWTToken<T extends string | object | Buffer>(payload: T): string | null {
+    let token: string
+
+    try {
+      token = jwt.sign(payload, SETTINGS.SECRET_KEY, {expiresIn: TOKEN_EXPIRES_IN})
+    } catch (error) {
+      console.log(error)
+
+      throw new Error('JWT token creating error')
+    }
+
+    if (!token) {
+      return null
+    }
+
+    return token
+  },
+  async verifyPassword(password: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(password, hash)
+  },
+  async getHash(password: string, userSalt?: string): Promise<{salt: string; hash: string}> {
+    const roundsNumber = 10
+    const salt = userSalt ?? (await bcrypt.genSalt(roundsNumber))
+
+    const hash = await bcrypt.hash(password, roundsNumber)
+
+    return {
+      salt,
+      hash,
+    }
+  },
+  getConfirmationCode(): string {
+    return crypto.randomUUID()
+  },
+}
